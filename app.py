c98de1f5113233c9f22e882793eebc552bf94256
Baig <baigzaid22@gmail.com>
2026-05-05 11:08:58 +0530

    fix: use PORT env variable for Render

--- app.py
+++ app.py
@@ -74,4 +74,5 @@ Answer:"""
     return jsonify({"answer": response.choices[0].message.content})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
